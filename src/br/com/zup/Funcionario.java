f42cb6562c0de7e508c4d8cff6aaab2dd0d10e63
Raline Silva <ralinesilva1@gmail.com>
2021-09-29 16:28:50 -0300

    Criar classe Funcionario

---
+++ src/br/com/zup/Funcionario.java
@@ -0,0 +1,22 @@
+package br.com.zup;
+
+public class Funcionario extends Pessoa{
+    private String codigoFuncionario;
+
+    public Funcionario() {
+
+    }
+
+    public Funcionario(String nome, String cpf, String codigoFuncionario) {
+        super(nome, cpf);
+        this.codigoFuncionario = codigoFuncionario;
+    }
+
+    public String getCodigoFuncionario() {
+        return codigoFuncionario;
+    }
+
+    public void setCodigoFuncionario(String codigoFuncionario) {
+        this.codigoFuncionario = codigoFuncionario;
+    }
+}
