0360da8f5c82d4c68f5ec488b0bf741354fb39bb
Michelle Kênia <keniamsoaress@gmail.com>
2021-09-30 19:36:26 -0300

    Capturar dados

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,5 +1,12 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class Sistema {
+
+    private static Scanner capturarDados(String mensagem) {
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
 
 }
