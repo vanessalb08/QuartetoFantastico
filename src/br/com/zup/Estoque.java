9e0eb4f6a809a2c1063dad31fecf31d133c4c7ef
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-30 16:16:07 -0300

    Reestruturar Classe Estoque

--- src/br/com/zup/Estoque.java
+++ src/br/com/zup/Estoque.java
@@ -4,43 +4,12 @@ import java.util.List;
 
 
 public class Estoque {
-    private Cliente cliente;
-    private Funcionario funcionario;
-    private int quantidadeDelivros;
+
     private List<Livro> listaDeLivros = new ArrayList<>();
 
 
     public Estoque() {
-    }
 
-    public Estoque(Cliente cliente, Funcionario funcionario, int quantidadeDelivros) {
-        this.cliente = cliente;
-        this.funcionario = funcionario;
-        this.quantidadeDelivros = quantidadeDelivros;
-    }
-
-    public Cliente getCliente() {
-        return cliente;
-    }
-
-    public void setCliente(Cliente cliente) {
-        this.cliente = cliente;
-    }
-
-    public Funcionario getFuncionario() {
-        return funcionario;
-    }
-
-    public void setFuncionario(Funcionario funcionario) {
-        this.funcionario = funcionario;
-    }
-
-    public int getQuantidadeDelivros() {
-        return quantidadeDelivros;
-    }
-
-    public void setQuantidadeDelivros(int quantidadeDelivros) {
-        this.quantidadeDelivros = quantidadeDelivros;
     }
 
     public List<Livro> getLivroslista() {
@@ -56,10 +25,8 @@ public class Estoque {
     @Override
     public String toString() {
         StringBuilder retorno = new StringBuilder();
-        retorno.append("\nCliente: \t" + cliente);
-        retorno.append("\nFuncionário: \t" + funcionario);
-        retorno.append("\nQuantidade de Livros: \t" + quantidadeDelivros);
-        retorno.append("Catálogo de livros: "+listaDeLivros);
+        retorno.append("\n Quantidade de Livros: \t"+listaDeLivros.size());
+        retorno.append("\n Catálogo de livros: \t"+listaDeLivros);
         return retorno.toString();
     }
 }
