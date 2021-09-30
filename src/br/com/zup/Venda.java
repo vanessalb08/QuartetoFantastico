a4487b7d1656e57713f75247acdcc77c39937744
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-30 16:20:35 -0300

    Corrigir conflitO

--- src/br/com/zup/Venda.java
+++ src/br/com/zup/Venda.java
@@ -1,23 +1,12 @@
 package br.com.zup;
 
 public class Venda {
-    private int tipovenda;
     private Livro livro;
 
     public Venda() {
+
     }
 
-    public Venda(int tipovenda) {
-        this.tipovenda = tipovenda;
-    }
-
-    public int getTipovenda() {
-        return tipovenda;
-    }
-
-    public void setTipovenda(int tipovenda) {
-        this.tipovenda = tipovenda;
-    }
     //Métodos
     //Acréscimo de 10% na compra a prazo
     public double venderAPrazo (){
@@ -25,8 +14,10 @@ public class Venda {
         livro.setPrecoLivro(novoValor);
         return novoValor;
     }
+
+
     //Desconto de 5% na compra a vista
-    public double venderAVista(){
+    public double venderAVista() {
         double novoValor = livro.getPrecoLivro() * 0.95;
         livro.setPrecoLivro(novoValor);
         return novoValor;
