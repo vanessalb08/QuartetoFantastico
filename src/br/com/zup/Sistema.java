a0683c88361dd96013ebe0106bf02e8241c28797
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-30 21:33:20 -0300

    AdICIONAR Livros na Lista

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,5 +1,7 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Sistema {
@@ -18,42 +20,66 @@ public class Sistema {
         System.out.println("Digite [4] para sair do sistema.");
     }
 
-    public static Livro cadastrarLivros(){
+    public static Livro cadastrarLivros() {
+
+
         System.out.println("=================================================================");
         String codigoISBN = capturarDados("Digite o Código do livro(ISBN): ").nextLine();
         String nomeDoLivro = capturarDados("Digite o Nome do livro: ").nextLine();
         String autor = capturarDados("Digite o autor do livro: ").nextLine();
         String editora = capturarDados("Digite a editora do livro: ").nextLine();
         double precoLivro = capturarDados("Digite o preço do livro").nextDouble();
-        Livro livrocadastrado = new Livro(codigoISBN,nomeDoLivro,autor,editora,precoLivro);
+        Livro livrocadastrado = new Livro(codigoISBN, nomeDoLivro, autor, editora, precoLivro);
         System.out.println("====================================================================");
-        System.out.println("Já tiramos o livro no deposito!!!");
+        System.out.println("\t Já tiramos o livro no deposito!!!");
         System.out.println("=====================================================================");
         System.out.println(livrocadastrado);
+
         return livrocadastrado;
     }
 
+    //Cadastrar Cliente pela venda
     public static Cliente cadastrarCliente(){
+        System.out.println("=================================================================");
         String nome = capturarDados("Digite o nome do cliente.").nextLine();
         String cpf = capturarDados("Digite o CPF do cliente.").nextLine();
         String telefone = capturarDados("Digite o telefone do cliente.").nextLine();
         Cliente cliente = new Cliente(nome,cpf,telefone);
-        System.out.println("Cliente cadastrado com sucesso!");
+        System.out.println("=================================================================");
+        System.out.println("\t\tCliente cadastrado com sucesso!");
+        System.out.println("=================================================================");
         return cliente;
     }
 
     //Cadastrar funcionário responsável pela venda
     public static Funcionario cadastrarFuncionario(){
-        System.out.println("------------------------------------------");
+        System.out.println("=================================================================");
         String nomeFuncionario = capturarDados("Digite o nome do funcionário que realizou a venda:").nextLine();
         String cpfFuncionario = capturarDados("Digite o cpf do funcionário:").nextLine();
         String telefoneFuncionario = capturarDados("Digite o telefone do cpf").nextLine();
 
         Funcionario funcionario = new Funcionario(nomeFuncionario,cpfFuncionario,telefoneFuncionario);
-        System.out.println("Funcionário cadastrado");
+        System.out.println("=================================================================");
+        System.out.println("\t\tFuncionário cadastrado");
+        System.out.println("=================================================================");
         System.out.println(funcionario);
+        System.out.println("=================================================================");
         return funcionario;
     }
+
+    public static List livrosNaLista() {
+        List<Livro> livrosL = new ArrayList<>();
+        System.out.println("*******************************************************");
+            int numLivros = capturarDados("Digite o número de livros a cadastrar").nextInt();
+            for(int i = 0;i<numLivros;i++) {
+                livrosL.add(cadastrarLivros());
+
+            }
+         return livrosL;
+    }
+
+
+
 
 
 }
